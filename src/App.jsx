4d9835a4c1ee61825be689cb6c03864535c9fc0d
محمد برجی <mohammad.programer.front@gmail.com>
2025-09-15 19:54:47 +0330

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+      <p className="text-amber-600">
+        Click on the Vite and React logos to learn more
+      </p>
+  )
+}
+
+export default App;
