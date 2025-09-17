5668e692423217310501463c62d03caa05191850
محمد برجی <mohammad.programer.front@gmail.com>
2025-09-17 12:04:12 +0330

    add respansive Menu Poshak

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFireAlt } from "react-icons/fa";
+import { AiOutlineSun } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa6";   //<FaMoon />
+import { TiThMenu } from "react-icons/ti";
+import { MdClose } from "react-icons/md";
+
+
 
 
 function Navbar() {
+
+const [Open,setOpen]=useState(false);
+const [Dark,setDark]=useState(false);
+
+
     return(
       <div className="bg-white p-2 w-full ">
         <nav className="flex flex-row justify-between ">
@@ -12,7 +23,7 @@ function Navbar() {
           </div>
 
           {/* منوی وسط */}
-          <div className="flex gap-6 justify-center ">
+          <div className="hidden md:flex gap-6 justify-center items-center ">
             <a href="#" className="hover:text-gray-500 transition">جدید ترین ها</a>
             <a href="#" className="hover:text-gray-500 transition">مردانه</a>
             <a href="#" className="hover:text-gray-500 transition">زنانه</a>
@@ -20,12 +31,34 @@ function Navbar() {
           </div>
 
           {/* دکمه در سمت چپ */}
-          <div className="flex justify-start ">
+          <div className="hidden md:flex justify-center items-center gap-3  ">
             <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition">
               خرید کنید
             </button>
+            <button onClick={()=>setDark(!Dark)} className="w-12 h-12 rounded-full hover:bg-gray-600 p-2 items-center justify-center my-auto mx-auto">
+             {Dark ?<AiOutlineSun size={20} className="mr-1.5" />:<FaMoon size={20} className="mr-1.5"/>}
+            </button>
           </div>
+          <div className="items-center justify-center my-auto mx-auto ml-4 p-4 md:hidden cursor-pointer">
+           <button className="" onClick={() => setOpen(!Open)}>
+           {Open ?<MdClose  size={30}/>:<TiThMenu size={30}/>}
+           </button>
+           
+          </div> 
         </nav>
+        {Open && (
+        <ul className="flex flex-col mt-4 space-y-4 items-center md:hidden">
+          <li><a href="#" className="hover:text-gray-500 transition">جدید ترین ها</a></li>
+          <li><a href="#" className="hover:text-gray-500 transition">مردانه </a></li>
+          <li><a href="#" className="hover:text-gray-500 transition">زنانه </a></li>
+          <li><a  href="#" className="text-red-600 flex"> حراج  <FaFireAlt size={20} /> </a></li>
+          <li><button onClick={()=>setDark(!Dark)} className="w-12 h-12 rounded-full hover:bg-gray-600 p-2 items-center justify-center my-auto mx-auto">
+             {Dark ?<AiOutlineSun size={20} className="mr-1.5" />:<FaMoon size={20} className="mr-1.5"/>}</button></li>
+          <li> <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition">
+              خرید کنید
+            </button></li>
+        </ul>
+      )}
       </div>
     )
 }
